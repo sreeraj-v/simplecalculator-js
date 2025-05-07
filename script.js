6e31a73017a831b859f0676df1b30380d62ed423
SREERAJ <168727746+sreeraj-v@users.noreply.github.com>
2025-05-07 23:11:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ function buttonclick(val){
   
 function clearDisplay(){
   document.getElementById("screen").value=""
-} 
- 
+}
 function equalClick(){
 var text= document.getElementById("screen").value
 var result =eval(text)
